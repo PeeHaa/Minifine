1d48fd6b988d8babd1a288a787a41817d74fd84f
PeeHaa <info@pieterhordijk.com>
2015-05-02 17:04:12 +0200

    Added invalid type exception

---
+++ src/InvalidTypeException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Exception which gets thrown when trying to prepend or append an invalid type
+ *
+ * PHP version 5.5
+ *
+ * @category   Minifine
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://pieterhordijk.com>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace MindwarpLib\Security\Encryption;
+
+/**
+ * Exception which gets thrown when trying to prepend or append an invalid type
+ *
+ * @category   Minifine
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidTypeException extends \Exception
+{
+}
