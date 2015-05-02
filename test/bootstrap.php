bc89d9601f600f2cbbdc97ef652545c663498397
PeeHaa <info@pieterhordijk.com>
2015-05-02 17:20:12 +0200

    Added tests bootstrap file

---
+++ test/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Bootstrap the tests
+ *
+ * PHP version 5.5
+ *
+ * @category   MinifineTest
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace MinifineTest;
+
+date_default_timezone_set('Europe/Amsterdam');
+
+/**
+ * Load PHPUnit
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
